fb6bc4f4d254788a74955e6cc4e2f10aa8b2fe85
Wallace Artur <wallacearthur_3@hotmail.com>
2024-10-15 18:29:08 -0300

    project created

---
+++ src/test/java/com/wallacearthur7/desafio_todolist/DesafioTodolistApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wallacearthur7.desafio_todolist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesafioTodolistApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
